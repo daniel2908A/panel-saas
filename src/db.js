852012f134ba89672e95e3142281c73f95416382
daniel2908A <danielqflores190@gmail.com>
2026-04-11 22:50:38 -0400

    use database_url final

--- src/db.js
+++ src/db.js
@@ -1,15 +1,10 @@
 const mysql = require('mysql2/promise');
 
-console.log("🔥 MYSQLHOST:", process.env.MYSQLHOST);
-console.log("🔥 MYSQLUSER:", process.env.MYSQLUSER);
-console.log("🔥 MYSQLDATABASE:", process.env.MYSQLDATABASE);
-
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = db;
