e34dbf79d624b7e06d9a6c6ef3f16e1872051bb0
daniel2908A <danielqflores190@gmail.com>
2026-04-11 22:07:56 -0400

    fix db variables real railway

--- src/db.js
+++ src/db.js
@@ -1,5 +1,14 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-module.exports = pool;
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+module.exports = db;
