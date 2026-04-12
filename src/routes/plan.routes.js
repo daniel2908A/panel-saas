383f0956f70652daf66a053c9533d3bd6cac2ac7
daniel2908A <danielqflores190@gmail.com>
2026-04-12 02:44:48 -0400

    fix: corregir ruta auth.middleware

--- src/routes/plan.routes.js
+++ src/routes/plan.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { subscribe } = require('../controllers/plan.controller');
-const authMiddleware = require('../middlewares/authMiddleware');
 
-// 🔥 CREAR SUSCRIPCIÓN
+const { subscribe } = require('../controllers/plan.controller');
+const authMiddleware = require('../middlewares/auth.Middleware');
+
+// 💳 SUSCRIPCIÓN
 router.post('/subscribe', authMiddleware, subscribe);
 
 module.exports = router;
