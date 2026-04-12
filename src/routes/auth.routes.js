8b86cff0f372c470541bd702d39c9a0ebc7cbd63
daniel2908A <danielqflores190@gmail.com>
2026-04-11 21:41:54 -0400

    fix auth routes syntax

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/auth.controller');
+const { login, register } = require('../controllers/auth.controller');
 
-// 🔐 LOGIN
-router.post('/login', authController.login);
-
-// 📝 REGISTRO
-router.post('/register', authController.register);
+router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
